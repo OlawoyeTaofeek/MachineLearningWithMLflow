afda9c94bd56c1536aae141fc233b9676451f09e
OlawoyeTaofeek <habephe21@gmail.com>
2025-02-10 23:51:57 +0100

    Just small Update for testing

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
 import os
+import logging
+import sys
+
+from src.mlProject.utils.logging_utils import setup_logging
+
+try:
+    log_file_path =setup_logging('practice.log')
+    logging.info(f"Logging is set up. Logs will be saved to {log_file_path}")
+except Exception as e:
+    print(f"Failed to set up logging: {e}")
+    sys.exit(1)
+    
+    
+import mlProject
+print(mlProject.__version__)
+

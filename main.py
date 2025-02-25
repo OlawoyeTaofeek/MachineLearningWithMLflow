6d0eba0c3c744b6809b64c349e6d128bb82bc4d3
OlawoyeTaofeek <habephe21@gmail.com>
2025-02-25 15:35:24 +0100

    Data Transformation

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from mlProject.utils.logging_utils import setup_logging
 from mlProject.pipeline.stage_01_ingestion import DataIngestionTrainingPipeline
 from mlProject.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
+from mlProject.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
 import logging
 
 
@@ -29,4 +30,19 @@ try:
     logging.info(f">>>>> {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
     logging.error(f"Error in {STAGE_NAME}: {e}")
+    raise e
+
+
+# ----------- Data Validation Stage -----------
+
+STAGE_NAME = "Data Transformation Stage"
+setup_logging("stage3_data_transformation.log")  # GLOBAL logging for this stage
+
+try:
+    logging.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    obj = DataTransformationTrainingPipeline()
+    obj.main()
+    logging.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logging.exception(e)
     raise e

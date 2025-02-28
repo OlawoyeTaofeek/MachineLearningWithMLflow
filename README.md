532087c08e5312eaad75e010afaee3587bd78fa1
OlawoyeTaofeek <habephe21@gmail.com>
2025-02-28 17:56:18 +0100

    Documentation and model experimenting

--- README.md
+++ README.md
@@ -1,1 +1,46 @@
-# Machine Learning With MLflow full End to End Project
+# 🏆 Machine Learning with MLflow - End-to-End Project
+
+This project implements a full **ML pipeline** using **MLflow** and other MLOps best practices. The pipeline covers **data ingestion, validation, model training, and deployment** with structured configuration management and logging.
+
+---
+
+## 🚀 How to Run?
+### **1️⃣ Clone the Repository**
+```bash
+https://github.com/OlawoyeTaofeek/MachineLearningWithMLflow
+
+cd MachineLearningWithMLflow
+```
+### 2️⃣ Create a Virtual Environment
+
+```bash
+python3.10 -m venv ML_env
+```
+
+```bash
+source ml_env/bin/activate  # On Windows use: ml_env\Scripts\activate
+```
+
+### 3️⃣ Install Dependencies
+```bash
+pip install -r requirements.txt
+```
+
+
+```bash
+# Finally run the following command
+python app.py
+```
+
+Now,
+```bash
+open up you local host and port
+```
+
+## MLflow
+
+[Documentation](https://mlflow.org/docs/latest/index.html)
+
+
+##### cmd
+- mlflow ui
--- main.py
+++ main.py
@@ -40,22 +40,22 @@ STAGE_NAME = "Data Transformation Stage"
 setup_logging("stage3_data_transformation.log")  # GLOBAL logging for this stage
 
 try:
-    logging.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    logging.info(f">>>>>> {STAGE_NAME} started <<<<<<")
     obj = DataTransformationTrainingPipeline()
     obj.main()
-    logging.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+    logging.info(f">>>>>> {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
     logging.exception(e)
     raise e
 
 # ---------------Model Training --------------------------------
 STAGE_NAME = "Model Trainer stage"
-setup_logging("stage_04_model_training.log")
+setup_logging("stage4_model_training.log")
 try:
-   logging.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   logging.info(f">>>>>> {STAGE_NAME} started <<<<<<") 
    data_ingestion = ModelTrainerTrainingPipeline()
    data_ingestion.main()
-   logging.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+   logging.info(f">>>>>> {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
         logging.exception(e)
         raise e
